fb9212b767e82e6d28591efcdb91836041848811
LuisUribe14 <luis.uribe253239@potros.itson.edu.mx>
2026-03-12 18:47:21 -0700

    aa

--- CineCompose_BuelnaDaniel_UribeLuis/app/src/main/java/buelna/daniel/cinecompose_buelnadaniel_uribeluis/ui/screens/PeliculaScreen.kt
+++ CineCompose_BuelnaDaniel_UribeLuis/app/src/main/java/buelna/daniel/cinecompose_buelnadaniel_uribeluis/ui/screens/PeliculaScreen.kt
@@ -1,6 +1,7 @@
 package buelna.daniel.cinecompose_buelnadaniel_uribeluis.ui.screens
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -9,6 +10,11 @@ import buelna.daniel.cinecompose_buelnadaniel_uribeluis.data.models.Pelicula
 
 @Composable
 fun PeliculaScreen() {
+    Column(
+
+    ) {
+        
+    }
 }
 
 @Preview(showBackground = true)
