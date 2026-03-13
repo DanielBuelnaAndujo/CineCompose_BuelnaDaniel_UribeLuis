7dc9455fef3f677042383b828bbf660d518ff29d
DanielBuelnaAndujo <daniel.buelna260378@potros.itson.edu.mx>
2026-03-13 01:48:15 -0700

    a

---
+++ CineCompose_BuelnaDaniel_UribeLuis/app/src/main/java/buelna/daniel/cinecompose_buelnadaniel_uribeluis/ui/screens/PeliculaScreen.kt
@@ -0,0 +1,18 @@
+package buelna.daniel.cinecompose_buelnadaniel_uribeluis.ui.screens
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import buelna.daniel.cinecompose_buelnadaniel_uribeluis.data.models.Pelicula
+
+@Composable
+fun PeliculaScreen() {
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PeliculaScreenPreview() {
+    PeliculaScreen()
+}
