6b7234923c920bc2cd2511feb48e515b7d2b5ada
DanielBuelnaAndujo <daniel.buelna260378@potros.itson.edu.mx>
2026-03-13 02:25:41 -0700

    Navigation

--- CineCompose_BuelnaDaniel_UribeLuis/app/src/main/java/buelna/daniel/cinecompose_buelnadaniel_uribeluis/ui/screens/DetallePelicula.kt
+++ CineCompose_BuelnaDaniel_UribeLuis/app/src/main/java/buelna/daniel/cinecompose_buelnadaniel_uribeluis/ui/screens/DetallePelicula.kt
@@ -1,2 +1,15 @@
 package buelna.daniel.cinecompose_buelnadaniel_uribeluis.ui.screens
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun DetallePelicula() {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DetallePeliculaScreen() {
+    DetallePelicula()
+}
