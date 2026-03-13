16a47f4f95e439d6eb1f7abeb8c05180653dc472
DanielBuelnaAndujo <daniel.buelna260378@potros.itson.edu.mx>
2026-03-13 01:24:59 -0700

    Proyecto creado

---
+++ CineCompose_BuelnaDaniel_UribeLuis/app/src/main/java/buelna/daniel/cinecompose_buelnadaniel_uribeluis/MainActivity.kt
@@ -0,0 +1,47 @@
+package buelna.daniel.cinecompose_buelnadaniel_uribeluis
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import buelna.daniel.cinecompose_buelnadaniel_uribeluis.ui.theme.CineCompose_BuelnaDaniel_UribeLuisTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CineCompose_BuelnaDaniel_UribeLuisTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CineCompose_BuelnaDaniel_UribeLuisTheme {
+        Greeting("Android")
+    }
+}
