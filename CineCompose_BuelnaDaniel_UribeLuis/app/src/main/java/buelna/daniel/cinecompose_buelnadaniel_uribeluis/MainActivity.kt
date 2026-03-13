1a9d5cc4a8015c0af522f5933eafaffdcfc710a0
DanielBuelnaAndujo <daniel.buelna260378@potros.itson.edu.mx>
2026-03-13 02:13:52 -0700

    Cambio Main viewModel()

--- CineCompose_BuelnaDaniel_UribeLuis/app/src/main/java/buelna/daniel/cinecompose_buelnadaniel_uribeluis/MainActivity.kt
+++ CineCompose_BuelnaDaniel_UribeLuis/app/src/main/java/buelna/daniel/cinecompose_buelnadaniel_uribeluis/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import buelna.daniel.cinecompose_buelnadaniel_uribeluis.ui.screens.PeliculaScreen
 import buelna.daniel.cinecompose_buelnadaniel_uribeluis.ui.theme.CineCompose_BuelnaDaniel_UribeLuisTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,6 +20,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CineCompose_BuelnaDaniel_UribeLuisTheme {
+                PeliculaScreen()
             }
         }
     }
--- CineCompose_BuelnaDaniel_UribeLuis/app/src/main/java/buelna/daniel/cinecompose_buelnadaniel_uribeluis/ui/screens/PeliculaScreen.kt
+++ CineCompose_BuelnaDaniel_UribeLuis/app/src/main/java/buelna/daniel/cinecompose_buelnadaniel_uribeluis/ui/screens/PeliculaScreen.kt
@@ -20,10 +20,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import buelna.daniel.cinecompose_buelnadaniel_uribeluis.viewmodel.PeliculaViewModel
 
 @Composable
-fun PeliculaScreen(viewModel: PeliculaViewModel) {
+fun PeliculaScreen(viewModel: PeliculaViewModel = viewModel()) {
     Column(
         modifier = Modifier
             .fillMaxSize()
